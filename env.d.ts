f52db591fdc9f0a53b510a29336cac7e3a3e8b80
Muhammadsolikh <sunnatovmuhammadsolikh@gmail.com>
2026-01-06 04:45:55 -0500

    fixed some bugs

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,25 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// Типы для vue3-leaflet (или @vue-leaflet)
+declare module '@vue-leaflet/vue-leaflet' {
+  export const LMap: any
+  export const LTileLayer: any
+  export const LMarker: any
+  export const LPopup: any
+}
+
+// Типы для ImportMeta (для import.meta.env)
+interface ImportMetaEnv {
+  readonly BASE_URL: string
+  // Добавь другие переменные окружения, если используешь (например, VITE_API_URL: string)
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
